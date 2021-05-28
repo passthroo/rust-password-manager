bba59e5d8894454800b8f4d592a489fab3cfd51e
passthroo <bvnqy89gb9jp345b4q@protonmail.com>
2021-05-28 00:56:26 -0700

    generates passwords with ChaCha20 CSPRNG, including CLI params

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,35 @@
 #[macro_use]
 extern crate clap;
 use clap::App;
+use rand::prelude::*;
+use rand_chacha::ChaCha20Rng;
 
 static LENGTH_DEFAULT: u64 = 20;
+
+fn generate(length: u64, special: bool) {
+    let mut rng = ChaCha20Rng::from_entropy();
+    let mut chars = String::from("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789");
+
+    if special {
+        // everything except " \
+        chars.push_str("~!@#$%^&*()_+`-=[];',./{}|:<>?");
+    }
+    
+    let mut result = String::from("");
+    for i in 0..length {
+        let idx = rng.gen_range(0..chars.len());
+        let pick = chars.chars().nth(idx).unwrap();
+        result.push(pick);
+    }
+
+    println!("{}", result);
+}
 
 fn main() {
     let yaml = load_yaml!("cli.yml");
     let matches = App::from_yaml(yaml).get_matches();
 
     if let Some(matches) = matches.subcommand_matches("generate") {
-        let alpha: bool = matches.is_present("alpha");
         let length: u64 = match matches.value_of("length") {
             Some(x) => x.parse().unwrap(),
             None    => LENGTH_DEFAULT,
@@ -17,13 +37,11 @@ fn main() {
         let special: bool = matches.is_present("special");
 
         println!("You want a password of length {:?}", length);
-        println!("You {}want alphanumeric characters", match alpha {
-            true    => "do not ",
-            false   => "",
-        });
         println!("You {}want special characters", match special {
             true    => "",
             false   => "do not ",
         });
+
+        generate(length, special);
     }
 }
