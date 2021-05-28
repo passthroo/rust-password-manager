bb1c689cf5c22d4dc8a3032a0dd42569b8401431
s.lim <sam@typhonic.io>
2021-05-28 15:29:14 -0700

    Format with rustfmt

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ fn generate(length: u64, special: bool) {
         // everything except " \
         chars.push_str("~!@#$%^&*()_+`-=[];',./{}|:<>?");
     }
-    
+
     let mut result = String::from("");
     for i in 0..length {
         let idx = rng.gen_range(0..chars.len());
@@ -32,15 +32,18 @@ fn main() {
     if let Some(matches) = matches.subcommand_matches("generate") {
         let length: u64 = match matches.value_of("length") {
             Some(x) => x.parse().unwrap(),
-            None    => LENGTH_DEFAULT,
+            None => LENGTH_DEFAULT,
         };
         let special: bool = matches.is_present("special");
 
         println!("You want a password of length {:?}", length);
-        println!("You {}want special characters", match special {
-            true    => "",
-            false   => "do not ",
-        });
+        println!(
+            "You {}want special characters",
+            match special {
+                true => "",
+                false => "do not ",
+            }
+        );
 
         generate(length, special);
     }
