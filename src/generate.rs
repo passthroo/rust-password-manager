be2cda6c9548adfdc081b988c4a5cebd01cfbddf
passthroo <bvnqy89gb9jp345b4q@protonmail.com>
2021-05-30 04:15:58 -0700

    rewrite generator to support different types of generation

--- src/generate.rs
+++ src/generate.rs
@@ -8,33 +8,53 @@ static CHARS_UPPER: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 static CHARS_NUMERIC: &str = "0123456789";
 static CHARS_SPECIAL: &str = "~!@#$%^&*()_+`-=[];',./{}|:<>?";
 
+pub struct Generator {
+    length: usize,
+    lower: bool,
+    upper: bool,
+    numeric: bool,
+    special: bool,
+}
+
 pub fn generate(length: Option<&str>, lower: bool, upper: bool, numeric: bool, special: bool) -> String {
-    let len;
+    let length = match length {
+        Some(x) => x.parse().unwrap(),
+        None    => DEFAULT_LENGTH,
+    };
+    let default = length == DEFAULT_LENGTH && !(lower || upper || numeric || special);
+    let lower = if default { true } else { lower };
+    let upper = if default { true } else { upper };
+    let numeric = if default { true } else { numeric };
+    let special = if default { true } else { special };
+    let generator = Generator { 
+        length: length,
+        upper: upper,
+        lower: lower,
+        numeric: numeric,
+        special: special,
+    };
+    return generate_nonsense(generator);
+}
 
-    // is length specified?
-    match length {
-        Some(x) => len = x.parse::<usize>().unwrap(),
-        None    => len = DEFAULT_LENGTH,
-    }
-
+fn generate_nonsense(generator: Generator) -> String {
     let mut chars = String::from("");
-    let mut result = String::with_capacity(len);
+    let mut result = String::with_capacity(generator.length);
     let mut rng = ChaCha20Rng::from_entropy();
 
-    if lower {
+    if generator.lower {
         chars.push_str(CHARS_LOWER);
     }
-    if upper {
+    if generator.upper {
         chars.push_str(CHARS_UPPER);
     }
-    if numeric {
+    if generator.numeric {
         chars.push_str(CHARS_NUMERIC);
     }
-    if special {
+    if generator.special {
         chars.push_str(CHARS_SPECIAL);
     }
 
-    for _ in 0..len {
+    for _ in 0..generator.length {
         let random_idx = rng.gen_range(0..chars.len());
         let pick = chars.chars().nth(random_idx).unwrap();
         result.push(pick);
@@ -43,11 +63,15 @@ pub fn generate(length: Option<&str>, lower: bool, upper: bool, numeric: bool, s
     return result;
 }
 
+fn generate_mnemonic(generator: Generator) -> String {
+    
+}
+
 mod tests {
     use super::*;
 
     #[test]
-    fn generate_default() {
+    fn generate_default_length() {
         let result = generate(None, true, true, true, true);
         assert_eq!(result.len(), DEFAULT_LENGTH);
     }
@@ -81,5 +105,10 @@ mod tests {
     fn generate_special() {
         let result = generate(None, false, false, false, true);
         assert!(result.chars().all(|x| String::from(CHARS_SPECIAL).contains(x)))
+    }
+
+    #[test]
+    fn generate_mnemonic() {
+        //TODO
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,13 @@ mod generate;
 fn main() {
     let yaml = load_yaml!("cli.yml");
     let matches = App::from_yaml(yaml).get_matches();
-
     if let Some(matches) = matches.subcommand_matches("generate") {
         let length = matches.value_of("length");
         let lower = matches.is_present("lower");
         let upper = matches.is_present("upper");
         let numeric = matches.is_present("numeric");
         let special = matches.is_present("special");
-        generate::generate(length, lower, upper, numeric, special);
+        let result = generate::generate(length, lower, upper, numeric, special);
+        println!("{}", result);
     }
 }
